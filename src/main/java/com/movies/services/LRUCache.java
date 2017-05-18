4c5f252527f8b68a0a026d054f54b5fedd5442a2
cedcori <cedrick.coriolan@gmail.com>
2017-05-18 09:29:19 +0200

    add ReadWriteLock

--- src/main/java/com/movies/services/LRUCache.java
+++ src/main/java/com/movies/services/LRUCache.java
@@ -1,14 +1,18 @@
 package com.movies.services;
 
 import java.util.LinkedHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-/**
- * For the LRUCache we can use the ReadWriteLock to read lock for get, and write
- * lock for put
- */
 public class LRUCache<K, V> {
 	private LinkedHashMap<K, V> map = new LinkedHashMap<K, V>();
 	private int limit;
+
+	private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+	private final Lock readLock = readWriteLock.readLock();
+	private final Lock writeLock = readWriteLock.writeLock();
 
 	public LRUCache(int limit) {
 		this.limit = limit;
@@ -19,27 +23,54 @@ public class LRUCache<K, V> {
 	}
 
 	public void put(K key, V val) {
-		if (map.size() >= limit) {
-			K keyToRemove = getFirtKeyRemovable();
-			map.remove(keyToRemove);
+		writeLock.lock();
+		try {
+			if (map.size() >= limit) {
+				K keyToRemove = getFirtKeyRemovable();
+				map.remove(keyToRemove);
+			}
+			map.put(key, val);
+		} finally {
+			writeLock.unlock();
 		}
-		map.put(key, val);
 	}
 
 	public int size() {
-		return map.size();
+		readLock.lock();
+		try {
+			return map.size();
+		} finally {
+			readLock.unlock();
+		}
 	}
 
 	public V get(K key) {
-		V oldValue = map.remove(key);
-		if (oldValue != null) {
-			map.put(key, oldValue);
+		readLock.lock();
+		try {
+
+			V oldValue = map.remove(key);
+			if (oldValue != null) {
+				map.put(key, oldValue);
+			}
+			return oldValue;
+		} finally {
+			readLock.unlock();
 		}
-		return oldValue;
+
 	}
 
 	public K getFirtKeyRemovable() {
-		return map.entrySet().iterator().next().getKey();
+		readLock.lock();
+		try {
+			if (!map.isEmpty()) {
+				return map.entrySet().iterator().next().getKey();
+			} else {
+				return null;
+			}
+
+		} finally {
+			readLock.unlock();
+		}
 	}
 
 }
